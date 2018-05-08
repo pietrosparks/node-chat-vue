94d0744eef8786a59345ae005fd679f9408f7986
Idakwoji Theophilus <pietrosparks@gmail.com>
2018-05-08 16:19:29 +0100

    change

--- api/config.js
+++ api/config.js
@@ -10,64 +10,67 @@ const serveStatic = require('serve-static');
 //CORS CONFIGURATION
 
 const incomingOriginWhitelist = [
-//for machines that use 'origin'
-'http://localhost:5000',
-//for machines that use 'host'
-'localhost:5000',
+  //for machines that use 'origin'
+  'http://localhost:5000',
+  //for machines that use 'host'
+  'localhost:5000',
 ]
 
 //CORS only takes requests hence the (req , next )
 //Takes the request checks the header and passes it on to the next process
 
 const corsConfig = (req, next) => {
-let corsOptions;
-let incomingOrigin = req.header('host') || req.header('origin');
-if (incomingOriginWhitelist.indexOf(incomingOrigin !== -1)) {
-  corsOptions = {
-    origin: true
-  }
-  return next(null, corsOptions);
-} else
-  corsOptions = {
-    origin: false
-  }
-return next(new Error('You like going under the hood, i like you. Contact me '))
+  let corsOptions;
+  let incomingOrigin = req.header('host') || req.header('origin');
+  if (incomingOriginWhitelist.indexOf(incomingOrigin !== -1)) {
+    corsOptions = {
+      origin: true
+    }
+    return next(null, corsOptions);
+  } else
+    corsOptions = {
+      origin: false
+    }
+  return next(new Error('You like going under the hood, i like you. Contact me '))
 
 }
 
 module.exports = (app, express) => {
 
-// const api = require('./routes/api')(express);
-//Initializing the Cors configuration 
-app.use(cors(corsConfig), (req, res, next) => {
-  next();
-})
-//View Engine Setup. But we are using Vue so no need 
+  // const api = require('./routes/api')(express);
+  //Initializing the Cors configuration 
+  app.use(cors(corsConfig), (req, res, next) => {
+    next();
+  })
+  //View Engine Setup. But we are using Vue so no need 
 
-//   app.set('views', path.join(__dirname, 'views'));
-//   app.set('view engine', 'ejs');
-// app.use('/api', api);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}))
-// app.use(history({verbose: true}))
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(logger('short'));
-  app.use(serveStatic(__dirname + "/dist"));
+  //   app.set('views', path.join(__dirname, 'views'));
+  //   app.set('view engine', 'ejs');
+  // app.use('/api', api);
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }))
+  // app.use(history({verbose: true}))
+  app.use('/',serveStatic(__dirname + "/../dist"));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(logger('short'));
+ 
 
-//catch errors 
-app.use((req, res, next) => {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-})
-// error handler 
-app.use((err, req, res) => {
-  res.locals.message = err.message
-  //Only prviding errors in development 
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  res.status(err.status || 500);
-  console.log(err);
-  res.render('error')
-})
+  //catch errors 
+  app.use((req, res, next) => {
+    var err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+  })
+  // error handler 
+  app.use((err, req, res) => {
+    res.locals.message = err.message
+    //Only prviding errors in development 
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    console.log(err);
+    res.render('error')
+  })
 
 }
